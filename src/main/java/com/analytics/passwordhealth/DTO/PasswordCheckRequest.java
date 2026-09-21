f8661fa18a473225744b9f29d7a233ede996d003
stu2401321013 <stu2401321013@uni-plovdiv.bg>
2026-09-21 23:25:51 +0300

    add global exception handler for user-friendly validation errors

--- src/main/java/com/analytics/passwordhealth/DTO/PasswordCheckRequest.java
+++ src/main/java/com/analytics/passwordhealth/DTO/PasswordCheckRequest.java
@@ -9,11 +9,11 @@ import jakarta.validation.constraints.Size;
 
 @Data
 public class PasswordCheckRequest {
-    @NotBlank(message = "Паролата не може да бъде празна или да съдържа само интервали!")
-    @Size(min = 8, max = 128, message = "Паролата трябва да бъде между 8 и 128 символа!")
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, max = 128, message = "Password length must be between 8 and 128 characters")
     @Pattern(
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
-            message = "Паролата трябва да съдържа поне една малка буква, една главна буква, една цифра и един специален символ (@$!%*?&)!"
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character (@$!%*?&)"
     )
     private String password;
 }
